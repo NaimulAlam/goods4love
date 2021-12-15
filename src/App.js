e7676605c909ca6a162975ee89a52ef7408e60bb
NaimulAlam <naimul2707@gmail.com>
2021-12-15 11:33:33 +0100

    not found page not showing resolved

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Homepage/Home/Home';
 import Login from './components/Login/Login';
+import NotFound from './components/NotFound/NotFound';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<RegistrationForm />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
