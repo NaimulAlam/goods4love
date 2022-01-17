5f8175532e50c6632cf8bbe6cc1c3bb5b334c316
bassiia <72402493+bassiia@users.noreply.github.com>
2022-01-17 18:48:30 +0100

    Add files via upload

---
+++ app/Models/AddDonation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AddDonation extends Model
+{
+    use HasFactory;
+    protected $table = 'add_donation';
+}
