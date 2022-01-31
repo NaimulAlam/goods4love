5e30e379a444e115b4045fdf95b5ac34f1bb7a0b
Mohmammad Naimul Alam <naimul2707@gmail.com>
2022-01-31 23:25:02 +0100

    some design bugs improved

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -5,12 +5,12 @@ import Navbar from '../../components/Navbar/Navbar';
 
 const NotFound = () => {
   return (
-    <div className="container-fluid text-center">
+    <div className="container-fluid text-center m-5 pt-5">
       <Navbar />
       <h2 style={{ color: 'red' }}> Sorry!</h2>
       <h2>URL Or Page Not Found</h2>
       <p>
-        <Link className="btn btn-info" to="/">
+        <Link className="btn btn-info btn-lg mt-5" to="/">
           Go Back
         </Link>
       </p>
