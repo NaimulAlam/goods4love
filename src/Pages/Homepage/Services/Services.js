30813a8f3c3a7da582da2d8c08e71166847d9ac6
Mohmammad Naimul Alam <naimul2707@gmail.com>
2022-02-01 15:26:35 +0100

    service aria unwanted code removed

--- src/Pages/Homepage/Services/Services.js
+++ src/Pages/Homepage/Services/Services.js
@@ -78,63 +78,6 @@ const Offers = () => {
         </div>
       </div>
     </div>
-    // <div className="container serviceDiv" id="Donate">
-    //   <div className="row">
-    //     <div className="col-md-4 col-sm-6 content-card">
-    //       <div className="card-big-shadow">
-    //         <div className="card card-just-text" data-background="color" data-color="blue" data-radius="none">
-    //           <div className="ServiceImageDiv">
-    //             <img className="ServiceImage" src={RWHeart} alt="" />
-    //           </div>
-    //           <div className="content">
-    //             <h6 className="category">Become a Valounteer</h6>
-    //             <h4 className="title">
-    //               <Link to="/#Valounteer">Valounteer</Link>
-    //             </h4>
-    //             <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    //           </div>
-    //         </div>
-    //         {/* <!-- end card --> */}
-    //       </div>
-    //     </div>
-
-    //     <div className="col-md-4 col-sm-6 content-card">
-    //       <div className="card-big-shadow">
-    //         <div className="card card-just-text" data-background="color" data-color="green" data-radius="none">
-    //           <div className="content">
-    //             <h6 className="category">Donate money</h6>
-    //             <h4 className="title">
-    //               <Link to="/#Donations">Donations</Link>
-    //             </h4>
-    //             <p className="description">
-    //               Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ducimus repellendus doloribus
-    //               doloremque natus quidem
-    //             </p>
-    //           </div>
-    //         </div>
-    //         {/* <!-- end card --> */}
-    //       </div>
-    //     </div>
-
-    //     <div className="col-md-4 col-sm-6 content-card">
-    //       <div className="card-big-shadow">
-    //         <div className="card card-just-text" data-background="color" data-color="purple" data-radius="none">
-    //           <div className="content">
-    //             <h6 className="category">Donate Items</h6>
-    //             <h4 className="title">
-    //               <Link to="/#Donations">Donations</Link>
-    //             </h4>
-    //             <p className="description">
-    //               Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ducimus repellendus doloribus
-    //               doloremque natus quidem
-    //             </p>
-    //           </div>
-    //         </div>
-    //         {/* <!-- end card --> */}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
